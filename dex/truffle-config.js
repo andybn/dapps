f57670e2d5ab7a6461b238ad4a87f689cb8061e2
Andy <andybn@gmail.com>
2021-12-21 18:50:31 +0100

    DEX-00002 Configuration

--- dex/truffle-config.js
+++ dex/truffle-config.js
@@ -4,6 +4,15 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 3500000
+    }
+  },
   compilers: {
     solc: {
       version: '0.8.0'
